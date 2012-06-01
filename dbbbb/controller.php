14f76de0c7992985d45ea69ab555829aaf383c78
Mike Lay <michael.lay.email@gmail.com>
2012-06-01 10:36:37 -0400

    Moves shutdown method and prepends css id
    Moves the shutdown method inside the package controller to simply setup
    and prepends #dbbbb-sql-log to reset and inline css

--- dbbbb/controller.php
+++ dbbbb/controller.php
@@ -51,57 +51,54 @@ SQL;
 		}
 
 		Database::setLogging(true);
-		Events::extend('on_render_complete', 'Dbbb', 'shutdown', __FILE__);
+		Events::extend('on_render_complete', 'DbbbbPackage', 'shutdown', __FILE__);
 	}
-}
-
-class Dbbb {
 
 	public static function shutdown() {
 
 		$reset = <<<RESET
-html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:'';content:none}table{border-collapse:collapse;border-spacing:0}
+#dbbbb-sql-log html,#dbbbb-sql-log body,#dbbbb-sql-log div,#dbbbb-sql-log span,#dbbbb-sql-log applet,#dbbbb-sql-log object,#dbbbb-sql-log iframe,#dbbbb-sql-log h1,#dbbbb-sql-log h2,#dbbbb-sql-log h3,#dbbbb-sql-log h4,#dbbbb-sql-log h5,#dbbbb-sql-log h6,#dbbbb-sql-log p,#dbbbb-sql-log blockquote,#dbbbb-sql-log pre,#dbbbb-sql-log a,#dbbbb-sql-log abbr,#dbbbb-sql-log acronym,#dbbbb-sql-log address,#dbbbb-sql-log big,#dbbbb-sql-log cite,#dbbbb-sql-log code,#dbbbb-sql-log del,#dbbbb-sql-log dfn,#dbbbb-sql-log em,#dbbbb-sql-log img,#dbbbb-sql-log ins,#dbbbb-sql-log kbd,#dbbbb-sql-log q,#dbbbb-sql-log s,#dbbbb-sql-log samp,#dbbbb-sql-log small,#dbbbb-sql-log strike,#dbbbb-sql-log strong,#dbbbb-sql-log sub,#dbbbb-sql-log sup,#dbbbb-sql-log tt,#dbbbb-sql-log var,#dbbbb-sql-log b,#dbbbb-sql-log u,#dbbbb-sql-log i,#dbbbb-sql-log center,#dbbbb-sql-log dl,#dbbbb-sql-log dt,#dbbbb-sql-log dd,#dbbbb-sql-log ol,#dbbbb-sql-log ul,#dbbbb-sql-log li,#dbbbb-sql-log fieldset,#dbbbb-sql-log form,#dbbbb-sql-log label,#dbbbb-sql-log legend,#dbbbb-sql-log table,#dbbbb-sql-log caption,#dbbbb-sql-log tbody,#dbbbb-sql-log tfoot,#dbbbb-sql-log thead,#dbbbb-sql-log tr,#dbbbb-sql-log th,#dbbbb-sql-log td,#dbbbb-sql-log article,#dbbbb-sql-log aside,#dbbbb-sql-log canvas,#dbbbb-sql-log details,#dbbbb-sql-log embed,#dbbbb-sql-log figure,#dbbbb-sql-log figcaption,#dbbbb-sql-log footer,#dbbbb-sql-log header,#dbbbb-sql-log hgroup,#dbbbb-sql-log menu,#dbbbb-sql-log nav,#dbbbb-sql-log output,#dbbbb-sql-log ruby,#dbbbb-sql-log section,#dbbbb-sql-log summary,#dbbbb-sql-log time,#dbbbb-sql-log mark,#dbbbb-sql-log audio,#dbbbb-sql-log video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}#dbbbb-sql-log article,#dbbbb-sql-log aside,#dbbbb-sql-log details,#dbbbb-sql-log figcaption,#dbbbb-sql-log figure,#dbbbb-sql-log footer,#dbbbb-sql-log header,#dbbbb-sql-log hgroup,#dbbbb-sql-log menu,#dbbbb-sql-log nav,#dbbbb-sql-log section{display:block}body{line-height:1}#dbbbb-sql-log ol,#dbbbb-sql-log ul{list-style:none}#dbbbb-sql-log blockquote,#dbbbb-sql-log q{quotes:none}#dbbbb-sql-log blockquote:before,#dbbbb-sql-log blockquote:after,#dbbbb-sql-log q:before,#dbbbb-sql-log q:after{content:'';content:none}#dbbbb-sql-log table{border-collapse:collapse;#dbbbb-sql-log border-spacing:0}
 RESET;
 
 		$css = <<<CSS
-h3 {
+#dbbbb-sql-log h3 {
 	border-top: 1px solid #333;
 	font-size: 22px;
 	font-weight: bold;
 	line-heigh: 1.5;
 	padding-top: 4px;
 }
-b {
+#dbbbb-sql-log b {
 	font-weight: bold;
 }
-font {
+#dbbbb-sql-log font {
 	font-size: 12px !important;
 }
-table {
+#dbbbb-sql-log table {
 	margin-top: 10px;
   border-bottom: 2px solid #333;
 	margin-bottom: 10px;
 }
-td {
+#dbbbb-sql-log td {
 	padding: 5px;
 }
-tr:nth-child(odd) {
+#dbbbb-sql-log tr:nth-child(odd) {
   background: #f0f0f0;
 }
-tr:nt-child(odd) td:nth-child(odd) {
+#dbbbb-sql-log tr:nt-child(odd) td:nth-child(odd) {
 	background: #eee;
 }
-tr:nth-child(even) {
+#dbbbb-sql-log tr:nth-child(even) {
 	background: #ccc;
 }
-tr:nth-child(even) td:nth-child(odd) {
+#dbbbb-sql-log tr:nth-child(even) td:nth-child(odd) {
 	background: #ddd;
 }
-tr:first-child {
+#dbbbb-sql-log tr:first-child {
 	color: #eee;
 	background: #333;
 }
-tr:first-child td:nth-child(odd) {
+#dbbbb-sql-log tr:first-child td:nth-child(odd) {
   background: #444;
 }
 #dbbbb-sql-log {
@@ -121,4 +118,5 @@ CSS;
 		echo '</div>';
 		$db->Execute('TRUNCATE TABLE adodb_logsql');
 	}
+
 }
